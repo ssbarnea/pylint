ssbarnea/pylint

commit 9d4f2c082e4f17fb5165deb0c9e4a0fd329d5cbd

Add checks for deprecated functions since 3.6

pylint/checkers/stdlib.py
@@ -215,6 +215,7 @@ class StdlibChecker(BaseChecker):
                 "base64.encodestring",
                 "base64.decodestring",
                 "ntpath.splitunc",
+                "os.path.splitunc",
             },
             (3, 2, 0): {
                 "cgi.escape",
@@ -244,10 +245,38 @@ class StdlibChecker(BaseChecker):
                 "fractions.gcd",
                 "inspect.formatargspec",
                 "inspect.getcallargs",
+                "inspect.getfullargspec",
                 "platform.linux_distribution",
                 "platform.dist",
             },
             (3, 6, 0): {"importlib._bootstrap_external.FileLoader.load_module"},
+            (3, 7, 0): {
+                "sys.set_coroutine_wrapper",
+                "sys.get_coroutine_wrapper",
+                "aifc.openfp",
+                "asyncio.Task.current_task",
+                "asyncio.Task.all_task",
+                "locale.format",
+                "ssl.wrap_socket",
+                "sunau.openfp",
+                "wave.openfp",
+            },
+            (3, 8, 0): {
+                "gettext.lgettext",
+                "gettext.ldgettext",
+                "gettext.lngettext",
+                "gettext.ldngettext",
+                "gettext.bind_textdomain_codeset",
+                "gettext.NullTranslations.output_charset",
+                "gettext.NullTranslations.set_output_charset",
+                "threading.Thread.isAlive",
+            },
+            (3, 9, 0): {
+                "binascii.b2a_hqx",
+                "binascii.a2b_hqx",
+                "binascii.rlecode_hqx",
+                "binascii.rledecode_hqx",
+            },
         },
     }
 

commit afff3265e91729a3a422edcc27f03c50fa9363b0

Fix regression about getfullargspec deprecation
Fixes regression introduced by
https://github.com/PyCQA/pylint/commit/9d4f2c082e4f17fb5165deb0c9e4a0fd329d5cbd which incorrectly added
getfullargspec as deprecated, which in fact was never deprecated,
not even in the latest 3.9.0

pylint/checkers/stdlib.py
@@ -165,7 +165,7 @@ class StdlibChecker(BaseChecker):
             ],
             (3, 5, 0): [
                 'fractions.gcd',
-                'inspect.getfullargspec', 'inspect.getargvalues',
+                'inspect.getargvalues',
                 'inspect.formatargspec', 'inspect.formatargvalues',
                 'inspect.getcallargs',
                 'platform.linux_distribution', 'platform.dist',